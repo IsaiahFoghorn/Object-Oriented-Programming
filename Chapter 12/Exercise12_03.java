a5908d8c98338b52396f84bc23ac79cf92292d36
IsaiahFoghorn <90852138+IsaiahFoghorn@users.noreply.github.com>
2022-03-31 09:02:46 -0600

    Update Exercise12-3

--- Chapter 12/Exercise12_03.java
+++ Chapter 12/Exercise12_03.java
@@ -1,7 +1,31 @@
 import java.util.*;
 
-class Exercise12_03 {
+class Exercise12_03 {	
+	public static int access(int x[], int i) {
+		if ((i < 0) || (i > 100)) {
+			throw new IndexOutOfBoundsException("Out of Bounds");
+		}
+		
+		return x[i];
+	}
+	
 	public static void main(String[] args) {
+		Scanner inp = new Scanner(System.in);
 		int[] x = new int[100];
+		
+		for (int i = 0; i < 100; i++) {
+			x[i] = (int)(Math.random() * 1000);
+		}
+		
+		
+		try {
+			System.out.print("");
+			int y = inp.nextInt();
+			System.out.print(x[y]);
+		}
+		catch (IndexOutOfBoundsException ex) {
+			System.out.print("Out of Bounds");
+		}
+		
 	}
 }
