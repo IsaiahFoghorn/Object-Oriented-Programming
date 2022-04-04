a1864e04426f7d7e4e898034ba888fee7792e7af
IsaiahFoghorn <90852138+IsaiahFoghorn@users.noreply.github.com>
2022-04-04 08:58:02 -0600

    Figured out the purpose of the throw statement.

--- Chapter 12/Exercise12_03.java
+++ Chapter 12/Exercise12_03.java
@@ -1,9 +1,9 @@
-import java.util.*;
+import java.util.Scanner;
 
 class Exercise12_03 {	
 	public static int access(int x[], int i) {
-		if ((i < 0) || (i > 100)) {
-			throw new IndexOutOfBoundsException("Out of Bounds");
+		if ((i < 0) || (i > 98)) {
+			throw new ArrayIndexOutOfBoundsException("Out of Bounds");
 		}
 		
 		return x[i];
@@ -12,6 +12,7 @@ class Exercise12_03 {
 	public static void main(String[] args) {
 		Scanner inp = new Scanner(System.in);
 		int[] x = new int[100];
+		int result;
 		
 		for (int i = 0; i < 100; i++) {
 			x[i] = (int)(Math.random() * 1000);
@@ -19,13 +20,13 @@ class Exercise12_03 {
 		
 		
 		try {
-			System.out.print("");
+			System.out.print("Test:");
 			int y = inp.nextInt();
-			System.out.print(x[y]);
+			access(x, y);
 		}
-		catch (IndexOutOfBoundsException ex) {
-			System.out.print("Out of Bounds");
+		catch (ArrayIndexOutOfBoundsException out) {
+			System.out.print(out);
 		}
-		
+		System.out.print("!");
 	}
 }
