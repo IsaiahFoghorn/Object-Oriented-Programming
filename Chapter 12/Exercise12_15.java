6e204787add0a4f99aabf99ce6b2dab08a0cfd04
IsaiahFoghorn <90852138+IsaiahFoghorn@users.noreply.github.com>
2022-04-06 09:03:03 -0600

    Base code for Exercise 12-15

--- Chapter 12/Exercise12_15.java
+++ Chapter 12/Exercise12_15.java
@@ -1,1 +1,25 @@
+import java.util.*;
+import java.io.*;
 
+class Exercise12_15 {
+	public static void main(String[] args) throws IOException {
+		java.io.File file = new java.io.File("Exercise12_15.txt");
+		
+		/**if (file.exists()) {
+			System.out.print("asdh");
+			System.exit(1);
+		}**/
+		
+		Scanner inp = new Scanner(file);
+		
+		//java.io.PrintWriter output = new java.io.PrintWriter(file);
+		
+		//output.print("a");
+		
+		//output.close();
+		
+		String test = inp.next();
+		
+		System.out.print(test);
+	}
+}
