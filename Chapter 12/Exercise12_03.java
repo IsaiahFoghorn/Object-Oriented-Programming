38e06f80244f63e8e646de9a45cdc0375e3f4eac
IsaiahFoghorn <90852138+IsaiahFoghorn@users.noreply.github.com>
2022-04-06 08:06:55 -0600

    Finished Exercise 12-3

--- Chapter 12/Exercise12_03.java
+++ Chapter 12/Exercise12_03.java
@@ -1,34 +1,34 @@
 import java.util.Scanner;
 
 class Exercise12_03 {	
-	public static int access(int x[], int i) {
-		if ((i < 0) || (i > 99)) {
+	public static int access(int array[], int i) {
+		if ((i < 0) || (i >= array.length)) {
 			throw new ArrayIndexOutOfBoundsException("Out of Bounds");
 		} else {
-			return x[i];
+			return array[i];
 		}
-		
-		
 	}
 	
 	public static void main(String[] args) {
 		Scanner inp = new Scanner(System.in);
 		int[] array = new int[100];
-		int result;
+		int result, exit = 0;
 		
 		for (int i = 0; i < 100; i++) {
 			array[i] = (int)(Math.random() * 1000);
 		}
 		
+		System.out.print("Please insert a number from 1 to 100: ");
 		
-		try {
-			System.out.print("Test:");
-			int answer = inp.nextInt();
-			access(array, answer);
-		}
-		catch (ArrayIndexOutOfBoundsException ex) {
-			System.out.print(ex);
-		}
-		System.out.print("!");
+		do {
+			try {
+				int answer = (inp.nextInt() - 1);
+				System.out.print(access(array, answer));
+				exit = 1;
+			}
+			catch (ArrayIndexOutOfBoundsException ex) {
+				System.out.print("Out of Bounds. \n\nPlease try again: ");
+			}
+		} while (exit == 0);
 	}
 }
