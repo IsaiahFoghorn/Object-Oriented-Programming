4d33828baf5594189030827cbe37ffb093716ef0
IsaiahFoghorn <90852138+IsaiahFoghorn@users.noreply.github.com>
2022-04-08 08:45:17 -0600

    Updated Exercise 12-15

--- Chapter 12/Exercise12_15.java
+++ Chapter 12/Exercise12_15.java
@@ -1,25 +1,39 @@
 import java.util.*;
 import java.io.*;
+import java.nio.file.*;
 
 class Exercise12_15 {
 	public static void main(String[] args) throws IOException {
 		java.io.File file = new java.io.File("Exercise12_15.txt");
+		int[] array = new int[100];
 		
-		/**if (file.exists()) {
-			System.out.print("asdh");
-			System.exit(1);
-		}**/
+		if (file.exists()) {
+			//System.out.print("asdh");
+			file.delete();
+			//System.exit(1);
+		}
+		
+		java.io.PrintWriter output = new java.io.PrintWriter(file);
+		
+		for (int i = 0; i < 100; i++) {
+			output.println((int)(Math.random() * 100) + 1);
+		}
+		
+		output.close();
 		
 		Scanner inp = new Scanner(file);
 		
-		//java.io.PrintWriter output = new java.io.PrintWriter(file);
+		for (int i = 0; i < array.length - 1; i++) {
+			array[i] = inp.nextInt();
+			System.out.print(array[i] + " ");
+		}
 		
-		//output.print("a");
-		
-		//output.close();
-		
-		String test = inp.next();
-		
-		System.out.print(test);
+		for (int i = 0; i < array.length - 1; i++) {
+			for (int j = i + 1; j < array.length - 1; j++) {
+				if (array[i] < array[j]) {
+					
+				}
+			}
+		}
 	}
 }
